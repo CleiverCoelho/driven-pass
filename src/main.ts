765a00b4747b6621425110af84eda84021f89027
Cleiver <creibis10@gmail.com>
2023-09-03 17:05:12 -0300

    fix: porta 300 harcoded mudada para port .env

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
+    console.log("Server is up and running on port: " + port);
+  });
 }
 bootstrap();
